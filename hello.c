b44dcebe55125df90851968f2ae350c31f754578
A-PS1999 <sarnoldparra@gmail.com>
2024-02-02 15:49:57 +0000

    Updated test.txt and hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+#define MAX_ITERS 10
+
 int main(void) {
 
     int i = 5;
     printf("Hello world!");
 
     printf("%d", i);
+
+    for (int j=0; j < MAX_ITERS; j++) {
+        i += j;
+        printf("Updated i = %d", i);
+    }
 
     return 0;
 }
